9f299630ac3694cf1286e53251cba04be32d8ffd
Alfieguerrero <alfrancis.guerrero@carleton.ca>
2018-03-25 12:31:01 -0400

    Player Count

--- GameIteration02/Assets/Scripts/GameManager.cs
+++ GameIteration02/Assets/Scripts/GameManager.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class GameManager : NetworkBehaviour {
-	public StoryDeck storyDeck;
-	public GameObject player;
-	public GameObject storyCard;
+	public StoryDeck 	storyDeck;
+	public GameObject 	storyCard;
+	GameObject[] 		storyCardDelete; 
 
+
+	void Start(){
+//		CmdAddPlayer (playerSize);
+		Debug.Log ("Player: " + netId.Value + " has joined.");
+	}
 	
 	// Update is called once per frame
 	void Update () {
@@ -16,14 +21,40 @@ public class GameManager : NetworkBehaviour {
 			return;
 		}
 	}
-
-	public void controlPlayerTurn(){
-
-	}
-		
-	public void PickUpStoryCard(){
+	
+	public void ControlPlayerTurn(){
 		if (!isLocalPlayer) {return;}
-		if (storyCard != null) {DestroyObject (storyCard);}
+		string playerTurnText = GameObject.FindGameObjectWithTag("PlayerTurnTextUI").GetComponent<Text>().text;
+		int playerTurnInt;
+		int.TryParse (playerTurnText, out playerTurnInt);
+		playerTurnInt++;
+
+		GameObject.FindGameObjectWithTag ("PlayerTurnTextUI").GetComponent<Text> ().text = playerTurnInt.ToString();
+//		Debug.Log ("LOCAL - Player: " + netId.Value + " " + playerTurnInt);
+		CmdControlPlayerTurn (this.gameObject, playerTurnInt.ToString());
+	}
+	[Command]
+	public void CmdControlPlayerTurn(GameObject gObject, string playerTurn){
+//		string playerTurnText = GameObject.FindGameObjectWithTag("PlayerTurnTextUI").GetComponent<Text>().text;
+//		int.TryParse (playerTurnText, out playerTurnInt);
+//		GameObject.FindGameObjectWithTag ("PlayerTurnTextUI").GetComponent<Text> ().text = playerTurnInt.ToString();
+//		Debug.Log ("CMD - Player: " + netId.Value + " " + playerTurnInt);
+		RpcControlPlayerTurn (this.gameObject, playerTurn);
+	}
+	[ClientRpc]
+	public void RpcControlPlayerTurn(GameObject gObject, string playerTurn){
+//		int.TryParse (playerTurnText, out playerTurnInt);
+		GameObject.FindGameObjectWithTag ("PlayerTurnTextUI").GetComponent<Text> ().text = playerTurn;
+		Debug.Log ("RPC - Player: " + netId.Value + " " + playerTurn);
+	}
+
+
+	public void PickUpStoryCard(){ // Local Button...
+		if (!isLocalPlayer) {return;}
+		storyCardDelete = GameObject.FindGameObjectsWithTag("StoryCard");
+		foreach (GameObject i in storyCardDelete){
+			DestroyObject (i);
+		}
 		string nameOfCard = storyDeck.NewCard ();
 		GameObject.FindGameObjectWithTag("StoryCardTextUI").GetComponent<Text>().text = nameOfCard;
 		storyCard = storyDeck.Draw(nameOfCard);		
@@ -31,20 +62,27 @@ public class GameManager : NetworkBehaviour {
 		storyCard.transform.localPosition = new Vector3 (-352f, 17f, 0f);
 		CmdPickUpStoryCard(this.gameObject, nameOfCard);
 	}
-
 	[Command] // Server calls Clients...
 	public void CmdPickUpStoryCard(GameObject gObject, string name){
-		if (storyCard != null) {DestroyObject (storyCard);}
+		storyCardDelete = GameObject.FindGameObjectsWithTag("StoryCard");
+		foreach (GameObject i in storyCardDelete){
+			DestroyObject (i);
+		}
 		RpcPickUpStoryCard(this.gameObject, name);
 	}
-
 	[ClientRpc] // Clients call Server... 
 	public void RpcPickUpStoryCard(GameObject gObject, string name){
-		if (storyCard != null) {DestroyObject (storyCard);}
-		GameObject.FindGameObjectWithTag("StoryCardTextUI").GetComponent<Text>().text = name;
+		storyCardDelete = GameObject.FindGameObjectsWithTag("StoryCard");
+		foreach (GameObject i in storyCardDelete){
+			DestroyObject (i);
+		}
 		storyCard = storyDeck.Draw(name);
 		storyCard.transform.SetParent (GameObject.Find ("GameCanvas").transform);
 		storyCard.transform.localPosition = new Vector3 (-352f, 17f, 0f);
+
+		GameObject.FindGameObjectWithTag("StoryCardTextUI").GetComponent<Text>().text = name;
+
+
 	}
 
 
